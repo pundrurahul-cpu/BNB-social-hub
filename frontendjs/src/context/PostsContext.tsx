f6e8e94b94fb2f1a1992522723bdf45d1605272f
pundirashul-cpu <pundirashul@gmail.com>
2026-09-06 17:59:53 +0530

    Fix: Implemented Reverse Proxy to bypass firewall without opening CloudPanel

--- frontendjs/src/context/PostsContext.tsx
+++ frontendjs/src/context/PostsContext.tsx
@@ -35,7 +35,7 @@ interface PostsContextType {
   refreshPosts: () => Promise<void>;
 }
 
-const API_URL = 'http://103.155.85.64:5001/api';
+const API_URL = '/api';
 
 const PostsContext = createContext<PostsContextType | undefined>(undefined);
 
